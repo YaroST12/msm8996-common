a171f7916322069da00a2d3c3325e4e0c0c234eb
Jorim Jaggi <jjaggi@google.com>
2018-04-04 17:27:54 +0200

    msm8996-common: enable early phase offsets
    Since we hit GL comp quite frequently during app transitions, we
    need to enable early phase offsets to give it enough time for
    composition.
    
    Test: Open app, make sure vsync-offsets shift
    Bug: 75985430
    Change-Id: I2f85bf72a4b135dce664f7274848f3956a9aaf7a
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- custom_prop.mk
+++ custom_prop.mk
@@ -3,6 +3,7 @@
 #
 
 PRODUCT_PROPERTY_OVERRIDES += \
+	debug.sf.early_phase_offset_ns=5000000 \
 	ro.af.client_heap_size_kbyte=7168 \
 	sdm.debug.rotator_downscale=1 \
 	persist.vendor.audio.hw.binder.size_kbyte=1024
