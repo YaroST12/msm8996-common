e410eed7ac17e17039fce70b990a25ed05897839
Vol Zhdanov <wight554@gmail.com>
2018-12-17 15:39:11 +0000

    msm8996-common: update memory props from OnePlus5T

--- custom_prop.mk
+++ custom_prop.mk
@@ -7,3 +7,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.af.client_heap_size_kbyte=7168 \
 	sdm.debug.rotator_downscale=1 \
 	persist.vendor.audio.hw.binder.size_kbyte=1024
+
+# Memory
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.heapmaxfree=8m \
+	dalvik.vm.heapminfree=4m \
+	dalvik.vm.heapsize=512m \
+	dalvik.vm.heapstartsize=16m \
+	dalvik.vm.heaptargetutilization=0.75 \
+	ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+	ro.vendor.qti.sys.fw.bservice_limit=5 \
+	ro.vendor.qti.sys.fw.bservice_age=5000
